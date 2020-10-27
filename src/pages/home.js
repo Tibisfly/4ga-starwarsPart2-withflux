244fab1466914a5e4c7f7b2806beddc22f41888d
Tibisfly <vielmatorotibisay@gmail.com>
2020-10-27 18:06:59 +0000

    star wars

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,21 +25,21 @@ export default function(props) {
                 {store.people.map((person, key)=>{
                 return (
                     <Cards tipo="character" title={person.name} urlID={getID(person.url)} key={key} cardImage="https://www.famefocus.com/wp-content/uploads/2017/05/conclusion-of-empire-1000x435.jpeg">
-                        <p>Gender: {person.gender}</p>
-                        <p>Hair-color: {person.hair_color} </p>
-                        <p>Eye-color: {person.eye_color}</p>
+                        <p  key={key}>Gender: {person.gender}</p>
+                        <p  key={key}>Hair-color: {person.hair_color}</p>
+                        <p  key={key}>Eye-color: {person.eye_color}</p>
                     </Cards>
                 )
                 })}
             </div>
             <Jumbotron title="Planets" />
             <div className="container-fluid d-flex cardScrollable">
-                {store.planets.map((planet, key)=>{
-                    return 
-                    <Cards tipo="planets" title={planet.name} urlID={getID(planet.url)} key={key} cardImage="https://static.wikia.nocookie.net/esstarwars/images/4/4c/TFA_galaxy_map.png/revision/latest?cb=20200204204632">
-                        <p>Population: {planet.population}</p>
-                        <p>Hair-color: {person.terrain}</p>
-                    </Cards>
+                {store.planets.map((planet, index)=>{
+                    return (
+                    <Cards tipo="planets" title={planet.name} urlID={getID(planet.url)} key={index} cardImage="https://static.wikia.nocookie.net/esstarwars/images/4/4c/TFA_galaxy_map.png/revision/latest?cb=20200204204632">
+                        <p  key={index}>Population: {planet.population}</p>
+                        <p  key={index}>Hair-color: {planet.terrain}</p>
+                    </Cards>)
                 })}
             </div>
         </div>
